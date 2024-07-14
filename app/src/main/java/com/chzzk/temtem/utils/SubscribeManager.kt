7227519868824094a4cbd578d37995afb2188d8e
unknown <dkstpwns10@naver.com>
2024-07-15 00:19:13 +0900

    q

--- app/src/main/java/com/chzzk/temtem/utils/SubscribeManager.kt
+++ app/src/main/java/com/chzzk/temtem/utils/SubscribeManager.kt
@@ -1,7 +1,12 @@
 package com.chzzk.temtem.utils
 
 import android.content.Context
+import androidx.datastore.dataStore
+import com.chzzk.temtem.utils.AppDataStore.Companion.dataStore
+import kotlinx.coroutines.flow.first
 
 class SubscribeManager(context: Context) {
+    val data = dataStore.data.first {
 
+    }
 }
